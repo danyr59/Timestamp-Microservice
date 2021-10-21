460047efe06025a2b53b738ddf47e8e0bbca28d2
danyr59 <danyelrange59@gmail.com>
2021-10-21 08:40:21 -0400

    changes

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.get("/", (req, res) => {
 })
 
 //API endpoint
+app.get("/api", (req, res) => {
+  let date = new Date();
+  res.json({ unix: date.valueof(), utc: date.toUTCString() })
+})
+
 app.get("/api/:dateTime", (req, res) => {
   const { dateTime: dateTimeString } = req.params;
   if (/\d{5,}/.test(dateTimeString)) {
